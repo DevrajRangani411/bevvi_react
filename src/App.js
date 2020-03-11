bcb39ae05c812489d4e8852dd30be6b669ba6cdb
DevrajRangani411 <devraj.rangani411@gmail.com>
2020-03-11 12:14:15 +0530

    create-react-app done.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/header';
+import Body from './components/body/body';
+import Footer from './components/footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
